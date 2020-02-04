821cd7f432610c08702ab80ddbaeb89a7193d659
AssanSav <assane.savadogo81@gmail.com>
2020-02-04 02:50:07 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(array, event) {
+    const resultArray = []
+    for (let i = 0; i < array.length; i++) {
+        resultArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+    return resultArray
+}
+
+function countDown(numbers) {
+    while (numbers >= 0) {
+        console.log(numbers)
+        numbers -= 1;
+    }
+}
