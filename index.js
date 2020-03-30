2f6b795a424b0f5d08605029115b5aff1dbc807a
AssanSav <assane.savadogo81@gmail.com>
2020-03-30 13:19:39 -0400

    Done.

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ function writeCards(array, event) {
     for (let i = 0; i < array.length; i++) {
         resultArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
     }
+    console.log(resultArray)
     return resultArray
 }
 
 function countDown(numbers) {
     while (numbers >= 0) {
-        console.log(numbers)
+        console.log(numbers )
         numbers -= 1;
     }
 }
+countDown(5)
+writeCards(["Ada", "Brendan", "Ali"], "birthday")
